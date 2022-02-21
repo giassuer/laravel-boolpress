73c7dc14d0f2c51f442e82724f8a926e0c1d0233
giassuer <g.assuero@hotmail.it>
2022-02-21 16:38:43 +0100

    work in progress

---
+++ resources/js/components/WorkInProgress.vue
@@ -0,0 +1,10 @@
+<template>
+    <h1>Work in progress</h1>
+</template>
+
+
+<script>
+export default {
+    name: 'WorkInProgress'
+}
+</script>
--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Auth::routes();
 
@@ -27,3 +24,8 @@ Route::middleware('auth')
         Route::get('/', 'HomeController@index')
         ->name('home');
     });
+
+
+Route::get('{any?}', function() {
+    return view('guests.home');
+})->where('any', '.*');
